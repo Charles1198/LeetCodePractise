1ead8f8a6fc45dfbdbad320d111a845a236fa787
charles <charles@bqteam.com>
2018-05-18 21:23:40 +0800

    add

--- src/com/bqteam/Main.java
+++ src/com/bqteam/Main.java
@@ -2,12 +2,35 @@ package com.bqteam;
 
 import com.bqteam.List.ListCode;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] s = new int[]{0, 0, 1, 2, 3, 3, 3, 4, 4};
 
         ListCode listCode = new ListCode();
-        listCode.removeDuplicates(s);
+
+//        int[] removeDuplicatesTestList = new int[]{1, 1, 2, 2, 2, 3, 4, 4, 4};
+//        listCode.removeDuplicates(removeDuplicatesTestList);
+//
+//        int[] maxProfitTestList = new int[]{1, 2, 3, 4, 5};
+//        listCode.maxProfit(maxProfitTestList);
+
+        int[] rotateTestList = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        println(rotateTestList);
+        listCode.rotate(rotateTestList, 2);
+        println(rotateTestList);
+    }
+
+    public static void print(Object o) {
+        System.out.print(o);
+    }
+
+    public static void println(Object o) {
+        System.out.println(o);
+    }
+
+    public static void println(int[] list) {
+        System.out.println(Arrays.toString(list));
     }
 }
